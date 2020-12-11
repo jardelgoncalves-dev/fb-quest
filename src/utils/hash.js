c86e3e3539fe8f9b6852552417e10b82303088c3
Jardel Gonçalves <jardelgoncalves1996@gmail.com>
2020-12-10 22:21:50 -0300

    feat: add generateToken and decodedToken

--- src/utils/hash.js
+++ src/utils/hash.js
@@ -1,4 +1,6 @@
 import bcrypt from 'bcrypt';
+import jwt from 'jsonwebtoken';
+import config from 'config';
 
 export class Hash {
   static generateHash(text = '', salt = 10) {
@@ -7,5 +9,13 @@ export class Hash {
 
   static compareHash(text, hash) {
     return bcrypt.compare(text, hash);
+  }
+
+  static generateToken(payload) {
+    return jwt.sign(payload, config.get('App.auth.key'));
+  }
+
+  static decodeToken(token) {
+    return jwt.verify(token, config.get('App.auth.key'));
   }
 }
