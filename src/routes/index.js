24def14f6bd4f3de9aba43a9882c1448ffedcd95
Jardel Gonçalves <jardelgoncalves1996@gmail.com>
2020-12-11 00:15:05 -0300

    fix: apply routes

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,11 +1,15 @@
-import { Router } from 'express';
+import express from 'express';
 import * as models from '@src/models';
 import { ServiceManager } from '@src/services';
 import usersRouter from './users.routes';
+import authRouter from './auth.routes';
 
 const serviceManager = new ServiceManager({ models });
-const router = Router();
+const router = express.Router();
 
-router.use(usersRouter(router, serviceManager));
+authRouter(router, serviceManager);
+usersRouter(router, serviceManager);
+
+router.get('/', (req, res) => res.send('Hello World!'));
 
 export default router;
