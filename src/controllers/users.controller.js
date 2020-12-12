abecc4d2b2bbc96f77c033fd8a884734262a525e
Jardel Gonçalves <jardelgoncalves1996@gmail.com>
2020-12-12 00:40:08 -0300

    feat: add find method to user returning

--- src/controllers/users.controller.js
+++ src/controllers/users.controller.js
@@ -6,5 +6,11 @@ export default (serviceManager) => {
       const response = await service.create({ data: req.body });
       res.status(response.status).json(response.data);
     },
+
+    async find(req, res) {
+      const _id = req.userId;
+      const response = await service.findOne({ query: { _id } });
+      res.status(response.status).json(response.data);
+    },
   };
 };
