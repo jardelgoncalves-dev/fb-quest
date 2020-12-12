31636323bdbe6a6336770ddfe908646e036c9627
Jardel Gonçalves <jardelgoncalves1996@gmail.com>
2020-12-12 14:58:41 -0300

    feat: add questions model

---
+++ src/models/questions.model.js
@@ -0,0 +1,63 @@
+import mongoose, { Schema } from 'mongoose';
+
+const schema = new Schema(
+  {
+    materia: {
+      type: String,
+      required: [true, 'materia é obrigatório'],
+    },
+    vestibular: {
+      type: String,
+      required: [true, 'vestibular é obrigatório'],
+    },
+    resolucao: {
+      type: String,
+      required: [true, 'resolucao é obrigatório'],
+    },
+    enunciado: {
+      type: String,
+      required: [true, 'enunciado é obrigatório'],
+    },
+    numeroQuestao: {
+      type: Number,
+      required: [true, 'numeroQuestao é obrigatório'],
+    },
+    ano: {
+      type: Number,
+      required: [true, 'ano é obrigatório'],
+    },
+    alternativas: [
+      {
+        letra: String,
+        correta: {
+          type: Boolean,
+          default: false,
+        },
+        texto: String,
+      },
+    ],
+    disponivel: {
+      type: Boolean,
+      default: true,
+    },
+  },
+  {
+    toJSON: {
+      transform: (_, ret) => {
+        ret.id = ret._id;
+
+        delete ret._id;
+        delete ret.__v;
+
+        ret.alternativas.forEach((r) => {
+          r.id = r._id;
+
+          delete r._id;
+          delete r.correta;
+        });
+      },
+    },
+  }
+);
+
+export const Question = mongoose.model('Question', schema);
