10b1f1822452417c4f4ab3d52b6e48095409c2ab
Jardel Gonçalves <jardelgoncalves1996@gmail.com>
2020-12-12 15:08:04 -0300

    feat: publish route question

--- src/routes/index.js
+++ src/routes/index.js
@@ -2,8 +2,10 @@ import express from 'express';
 import * as models from '@src/models';
 import { ServiceManager } from '@src/services';
 import { authMiddleware as auth } from '@src/middlewares/auth.middleware';
+import { isAdmin } from '@src/middlewares/isAdmin.middleware';
 import usersRoutes from './users.routes';
 import authRoutes from './auth.routes';
+import questionsRoutes from './questions.routes';
 
 const serviceManager = new ServiceManager({ models });
 
@@ -12,5 +14,9 @@ const router = express.Router();
 router.get('/', (req, res) => res.send('welcome to fb quest!'));
 router.use('/users', usersRoutes(express.Router, serviceManager, { auth }));
 router.use('/auth', authRoutes(express.Router, serviceManager));
+router.use(
+  '/questions',
+  questionsRoutes(express.Router, serviceManager, { isAdmin, auth })
+);
 
 export default router;
