22db1b09111b361defd26bb6a4d7db936d31796c
Jardel Gonçalves <jardelgoncalves1996@gmail.com>
2020-12-13 10:21:14 -0300

    feat: add new routes

--- src/routes/exams.routes.js
+++ src/routes/exams.routes.js
@@ -3,10 +3,15 @@ import ExamsController from '@src/controllers/exams.controller';
 export default (routerInstance, serviceManager, middlewares = {}) => {
   const router = routerInstance();
 
-  const questionController = ExamsController(serviceManager);
+  const examsController = ExamsController(serviceManager);
 
   router.use(middlewares.auth);
-  router.post('/', questionController.store);
-
+  router.get('/', examsController.index);
+  router.get('/:id', examsController.show);
+  router.get(
+    '/:examId/questao/:questionId/alternativa/:alternativaId/verifica',
+    examsController.verify
+  );
+  router.post('/', examsController.store);
   return router;
 };
