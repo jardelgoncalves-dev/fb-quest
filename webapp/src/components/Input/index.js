335d4f6df8a0feecf5bb239215c02187119cff92
Jardel Gonçalves <jardelgoncalves1996@gmail.com>
2020-12-17 20:34:33 -0300

    refactor(webapp): add onChange

--- webapp/src/components/Input/index.js
+++ webapp/src/components/Input/index.js
@@ -1,8 +1,9 @@
 import React from 'react';
 import { Wrapper } from './styles';
 
-export const Input = ({ value, placeholder, name, type = 'text' }) => (
+export const Input = ({ value, placeholder, name, type = 'text', onChange }) => (
   <Wrapper
+    onChange={onChange}
     value={value}
     name={name}
     placeholder={placeholder}
