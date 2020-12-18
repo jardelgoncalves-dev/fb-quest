8ff8a71aa242f93b357461adca998d8dc6132b8f
Jardel Gonçalves <jardelgoncalves1996@gmail.com>
2020-12-18 01:59:31 -0300

    refactor(webapp): add redirect if status code is 401

--- webapp/src/components/ErrorMessage/index.js
+++ webapp/src/components/ErrorMessage/index.js
@@ -1,10 +1,16 @@
 import React from 'react';
+import { Redirect } from 'react-router-dom';
 import { Wrapper } from './styles';
 
 export const ErrorMessage = ({ error }) => {
   const handlerError = (error) => {
-    if (error.response && error.response.data && error.response.data.error) {
-      return error.response.data.error
+    if (error.response) {
+      if(error.response.status && error.response.status === 401) {
+        return <Redirect to="/" />
+      }
+      if (error.response.data && error.response.data.error) {
+        return error.response.data.error
+      }
     }
     return 'Erro interno'
   }
