438599f53a528bfa1e8b6197fc5988602cc24073
Jardel Gonçalves <jardelgoncalves1996@gmail.com>
2020-12-19 02:20:36 -0300

    feat: add bg color prop

--- webapp/src/components/Button/index.js
+++ webapp/src/components/Button/index.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import { Wrapper } from './styles';
 
-export const Button = ({ text, onClick, width, disabled }) => (
+export const Button = ({ text, onClick, width, disabled, bg }) => (
   <Wrapper
     onClick={onClick}
     width={width}
     disabled={disabled}
+    bg={bg}
   >
     {text}
   </Wrapper>
